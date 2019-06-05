e08985026fc06fb02c35d2c59d659be5628349f8
Anoushka Sharma <35841938+Chinkiizz@users.noreply.github.com>
2019-06-05 12:03:24 +0530

    Add files via upload

--- recursion3.cpp
+++ recursion3.cpp
@@ -1,22 +1,31 @@
+//this is an example o infinite loop using recursion
+//If we initialise j=50 it wil always return a string "something" will be printed and
+return 0 will never be reached.
+
+
 #include<iostream>
-#include<conio.h>
+ #include<conio.h>
+  using namespace std;
 
+  int f(int j)
+  {
+  static int i=50;
+  int k;
+  if(i==j)
+  {
+  cout<<"\nSomething\n";
+  k=f(i);
+  return 0;
 
- using namespace std;
- int factorial(int n)
- {
+  }
+  else return 0;
 
- if(n==0)
- return 1;
+  }
+  int main()
+  {
+  cout<<"Hi";
 
- return n*factorial(n-1);
- }
+  f(50);
 
- int main()
- {
-
- int x=4;
- cout<<"Factorial of the number "<<x<< " is "<< factorial(4);
-
-
- return 0;}
+  getch();
+  }
