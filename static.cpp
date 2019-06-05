20ee772bb23457dc5fff652f9202bf4232e638c0
Anoushka Sharma <35841938+Chinkiizz@users.noreply.github.com>
2019-06-05 11:51:30 +0530

    Add files via upload

---
+++ static.cpp
@@ -0,0 +1,23 @@
+//static and global variables in Recursion
+
+#include<iostream>
+ #include<conio.h>
+ using namespace std;
+
+ int fun(int n)
+ {
+ static int x=0;
+ if(n>0)
+ {
+ x++;
+ return fun(n-1)+x;
+ }
+ return 0;
+ }
+
+ int main()
+ {
+ int r;
+ r=fun(5);
+ cout<<endl<<r;
+ return 0;}
