3acdc04e9205f6ff101f8489747ad6f766f56f64
Anoushka Sharma <35841938+Chinkiizz@users.noreply.github.com>
2019-07-07 10:09:25 +0530

    Add files via upload

---
+++ print_add_var.c
@@ -0,0 +1,12 @@
+//print the address of a variable
+
+#include<stdio.h>
+ int main()
+ {
+
+ int a=10;
+ printf(" This is the value of variable : %d", a);
+printf("\n");
+ printf("%p", a);
+ return 0;
+ }
