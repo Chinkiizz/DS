9286ed96f353ecd5afb86128e53b7d23ce38c9be
Anoushka Sharma <35841938+Chinkiizz@users.noreply.github.com>
2019-08-07 10:23:22 +0530

    Add files via upload

--- diagonal.c
+++ diagonal.c
@@ -1,3 +1,7 @@
+/* anywhere in your code if you need diagonal matrix, then you need a structure to define all its entities like shown below
+ no need to store the diagonal matrix in a 2 d array, instead we store it in a 1D array, storing only the non-zero elements.
+ */
+
 #include<stdio.h>
 struct diagmatrix{
     int A[10];
@@ -38,5 +42,7 @@ void Display(struct diagmatrix m ){
      set(&m, 2,2,8);
      set(&m, 3,3,9);
      set(&m, 4,4,12);
+     printf("%d \n", get(m,2,2));
+     printf("%d \n", get(m, 3,3));
      Display(m);
  return 0;}
